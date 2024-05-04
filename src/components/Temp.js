bc9ebf8666a6c877ed90d46e06bd704e69721fb4
Shalini Biswas <shalinibiswas003@gmail.com>
2024-05-04 23:16:09 +0530

    Under construction commit

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -4,7 +4,7 @@ import './Temp.css'; // Import CSS file for styling
 function Temp() {
   return (
     <div className="construction-message">
-      <p>* The site is under construction *</p>
+      <p className='first'>* The site is under construction *</p>
       <p className='second'>* The site is under construction *</p>
      
     </div>
