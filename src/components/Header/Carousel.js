6ccc6a28754d1f6dd4c59229e5174b6d4b4130e8
abhinabphy <abhinabiitg@gmail.com>
2024-05-14 20:08:30 +0530

    updated swiper

--- src/components/Header/Carousel.js
+++ src/components/Header/Carousel.js
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/swiper-bundle.css'; // Import Swiper styles
 import comm from '../../assets/img/community.jpg';
 import focus from '../../assets/img/focused.jpg';
+import invention from '../../assets/img/invention.jpg';
 import Nav from '../NavNew/Nav';
 import Temp from '../Temp';
 import classes from './Carousel.css';
@@ -35,17 +36,26 @@ const Carousel = () => {
           <SwiperSlide>
             <img className="image" src={comm} alt="Image 1" style={{ height: "100vh", width: "100vw" }} />
             <div className="carousel-text ">
-              <h2>Slide 1</h2>
-              <p>Text for slide 1</p>
+              <h2>Transforming Ideas into Reality</h2>
+              <p>Welcome to IQApex Labs, your one-stop solution to all your IT service and software development needs. We specialize in web and mobile app development, blockchain development, and digital marketing. Our team of experts is dedicated to providing you with innovative, research-based solutions that transform your ideas into reality. Let us help you take your business to the next level.</p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <img className="image" src={focus} alt="Image 2" style={{ height: "100vh", width: "100vw" }} />
             <div className="carousel-text">
-              <h2>Slide 2</h2>
-              <p>Text for slide 2</p>
+              <h2>Your Passion, Our Creation</h2>
+              <p>We provide tailored digital marketing solutions that help businesses increase their online visibility, engage their target audience, and drive conversions.</p>
             </div>
           </SwiperSlide>
+          <SwiperSlide>
+            <img className="image" src={invention} alt="Image 3" style={{ height: "100vh", width: "100vw" }} />
+            <div className="carousel-text">
+              <h2>Research-based Product Development</h2>
+              <p>We provide research-based product development expertise that helps businesses bring innovative products to market quickly and cost-effectively.
+</p>
+            </div>
+          </SwiperSlide>
+
         </div>
         
       </Swiper>
