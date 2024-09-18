d0517afcf25e93a1c73c9a949863dc924e6364f6
Iqapex <103753966+Iqapex@users.noreply.github.com>
2024-09-19 04:06:52 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-// import logo from '../../assets/img/logo-footer.png'
 import { FaFacebook, FaInstagram, FaLinkedinIn, FaMapMarkerAlt } from 'react-icons/fa'
 import { GrMail } from 'react-icons/gr'
 import { MdCall } from 'react-icons/md'
@@ -13,65 +12,55 @@ const Footer = () => {
   const {mouseEnterHandler, mouseLeaveHandler} = useContext(mouseContext)
 
   const expertiseArr = [['Product Engineering', 'product-engineering'], ['Expertise Solutions', 'expertise-solutions'], ['Talent Partnership', 'talent-partnership']]
-  // const usefulArr = [['About Us', 'about-us'], ['Blogs', 'blogs'], ['Careers', 'careers']]
-  const usefulArr = [['Careers', 'careers'],['Announcements'],['Parters with us']]
-  const industries=[['Information Technology'],['E-commerce & Retail'],['AR/VR in Manufacturing'],['Travel & Logistics'],['Sustainability & Climate change']];
-  const prod=[['IT & Consulting'],['Creative Design','services'],['Digital Marketing & Branding'],['Website Development','services'],['Mobile App Development','services'],['IQVerse','iqverse']]
+  const usefulArr = [['Careers', 'careers'], ['Announcements'], ['Partners with us']]
+  const industries = [['Information Technology'], ['E-commerce & Retail'], ['AR/VR in Manufacturing'], ['Travel & Logistics'], ['Sustainability & Climate change']]
+
+  const addresses = [
+    "Shyamnagar, Kolkata, West Bengal, India",
+    "Sector V, Kolkata, West Bengal, India",
+    "Jayanagar, Karnataka, India, 560011",
+    "Rajendranagar, Hyderabad, India, 500030",
+    "Borjhar, Guwahati near Airport, India, 781015"
+  ]
 
   return (
     <footer className={classes['footer-container']}>
-
-        <div className={classes['footer-content']}>
+      <div className={classes['footer-content']}>
+        <div className={classes['footer-columns']}>
+          <FooterCard title='Our Expertise' links={expertiseArr} />
+          <FooterCard title='Useful Links' links={usefulArr} />
+          <FooterCard title="Industries" links={industries}/>
           
-
-          <div className={classes['card-container']}>
-            
-           
-            <div className={`${classes['footer-card']} ${classes.address}`}>
-              <h3>Get in Touch</h3>
-              <div className={classes.info}>
-                <div className={classes['info-card']}>
-                    <span className={classes['circle-icon']}><FaMapMarkerAlt className={classes.icon} /></span>
-                    <p>
-                      <span>Address 1 - </span>1-A, Mohan Kunja Apartment, Ground Floor, Biman Nagar, Kaikhali, Kolkata, West Bengal 700052 
-                      <br /><br />
-                      <span>Address 2 - </span>Ashutosh Nagar, Vivekananda Road, North 24 Parganas, Barrackpore, West Bengal 743133
-                    </p>
+          <div className={`${classes['footer-card']} ${classes.address}`}>
+            <h3>Get in Touch</h3>
+            <div className={classes.info}>
+              {addresses.map((address, index) => (
+                <div key={index} className={classes['info-card']}>
+                  <span className={classes['circle-icon']}><FaMapMarkerAlt className={classes.icon} /></span>
+                  <p>
+                    <span></span>{address}
+                  </p>
                 </div>
-                <div className={classes['info-card']}>
-                    <span className={classes['circle-icon']}><GrMail className={classes.icon} /></span>
-                    <p>contact@iqapexlabs.com</p>
-                </div>
-                <div className={classes['info-card']}>
-                    <span className={classes['circle-icon']}><MdCall className={classes.icon} /></span>
-                    <p>+91 62895 69385</p>
-                </div>
+              ))}
+              <div className={classes['info-card']}>
+                <span className={classes['circle-icon']}><GrMail className={classes.icon} /></span>
+                <p>contact@iqapexlabs.com</p>
               </div>
-              <div className={classes['social-media']}>
-                <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.facebook.com/IQApexLabs" target="_blank" rel="noreferrer"><FaFacebook className={classes.fb}/></a></span>
-
-                <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.instagram.com/iqapexlabs" target="_blank" rel="noreferrer"><FaInstagram className={classes.insta}/></a></span>
-                
-                <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.linkedin.com/company/iqapex-labs/" target="_blank" rel="noreferrer"><FaLinkedinIn className={classes.linkedin} /></a></span>
+              <div className={classes['info-card']}>
+                <span className={classes['circle-icon']}><MdCall className={classes.icon} /></span>
+                <p>+91 62895 69385</p>
               </div>
             </div>
-
+            <div className={classes['social-media']}>
+              <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.facebook.com/IQApexLabs" target="_blank" rel="noreferrer"><FaFacebook className={classes.fb}/></a></span>
+              <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.instagram.com/iqapexlabs" target="_blank" rel="noreferrer"><FaInstagram className={classes.insta}/></a></span>
+              <span onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}><a href="https://www.linkedin.com/company/iqapex-labs/" target="_blank" rel="noreferrer"><FaLinkedinIn className={classes.linkedin} /></a></span>
+            </div>
           </div>
-
-
-
         </div>
-        <div className="cards" style={{display:"flex",gap:"3rem",margin:"2rem",flexWrap:"wrap",justifyContent:"space-evenly"}}>
-            <FooterCard title='Our Expertise' links={expertiseArr} />
-            <FooterCard title='Useful Links' links={usefulArr} />
-            <FooterCard title="Industries" links={industries}/>
-            
-            
+      </div>
 
-        </div>
-
-        <p className={classes.copyright}>&copy; 2024 IQApex Labs, All Rights Reserved</p>
-
+      <p className={classes.copyright}>&copy; 2024 IQApex Labs, All Rights Reserved</p>
     </footer>
   )
 }
